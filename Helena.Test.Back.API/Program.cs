58b7eee79a9bb5bbeec28fcd56e5ab86dab5f23a
Vini <vinicius.rs97@hotmail.com>
2025-04-13 18:50:48 -0300

    Add Services e Repository  Generic para poder maepar automaticamento todas novas services e repository futuras, assim não precisa add um novo addscoped em program.

--- Helena.Test.Back.API/Program.cs
+++ Helena.Test.Back.API/Program.cs
@@ -1,8 +1,11 @@
+using Helena.Test.Back.BootstrapApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddServicesAndRepositories();
 
 var app = builder.Build();
 
---
+++ Helena.Test.Back.BootstrapApp/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Helena.Test.Back.Repository.Interfaces;
+using Helena.Test.Back.Service.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using NetCore.AutoRegisterDi;
+using System.Reflection;
+
+namespace Helena.Test.Back.BootstrapApp
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServicesAndRepositories(this IServiceCollection services)
+        {
+            services
+             .RegisterAssemblyPublicNonGenericClasses(
+                Assembly.GetAssembly(typeof(IGenericRepository)),
+                Assembly.GetAssembly(typeof(IGenericService))
+              )
+             .Where(c => c.Name.EndsWith("Service") || c.Name.EndsWith("Repository"))
+             .AsPublicImplementedInterfaces(ServiceLifetime.Scoped);
+
+            return services;
+        }
+    }
+}
