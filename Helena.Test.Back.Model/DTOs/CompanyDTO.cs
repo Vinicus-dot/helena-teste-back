b033e7b3d088c8f3eb69aeab9ed2e8d818e94509
Vini <vinicius.rs97@hotmail.com>
2025-04-13 19:27:57 -0300

    Model Company Entity e DTO

---
+++ Helena.Test.Back.Model/DTOs/CompanyDTO.cs
@@ -0,0 +1,46 @@
+﻿using Helena.Test.Back.Model.Entitys;
+using System.Text.Json.Serialization;
+
+namespace Helena.Test.Back.Model.DTOs
+{
+    public class CompanyDTO
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("avatarUrl")]
+        public string AvatarUrl { get; set; }
+
+        [JsonPropertyName("nomeFantasia")]
+        public string NomeFantasia { get; set; }
+
+        [JsonPropertyName("razaoSocial")]
+        public string RazaoSocial { get; set; }
+
+        [JsonPropertyName("qtdeFuncionarios")]
+        public int QtdeFuncionarios { get; set; }
+
+        [JsonPropertyName("active")]
+        public bool Active { get; set; }
+
+    }
+
+    public static class CompanyExtensions
+    {
+        public static CompanyDTO ToDto(this CompanyEntity entity)
+        {
+            if (entity == null)
+                return new CompanyDTO();
+
+            return new CompanyDTO
+            {
+                Id = entity.Id,
+                AvatarUrl = entity.AvatarUrl,
+                NomeFantasia = entity.NomeFantasia,
+                RazaoSocial = entity.RazaoSocial,
+                QtdeFuncionarios = entity.QtdeFuncionarios,
+                Active = entity.Active
+            };
+        }
+    }
+}
---
+++ Helena.Test.Back.Model/Entitys/CompanyEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Helena.Test.Back.Model.Entitys
+{
+    public class CompanyEntity
+    {
+        public int Id { get; set; }
+        public string AvatarUrl { get; set; }
+        public string NomeFantasia { get; set; }
+        public string RazaoSocial { get; set; }
+        public int QtdeFuncionarios { get; set; }
+        public bool Active { get; set; }
+    }
+
+}
