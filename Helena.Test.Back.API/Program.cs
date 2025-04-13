f7e8de737b2b56b567f8e179fe371af80cfeea17
Vini <vinicius.rs97@hotmail.com>
2025-04-13 20:58:02 -0300

    habilita CORS para qualquer origem com possibilidade futura de restringir por IP

--- Helena.Test.Back.API/Program.cs
+++ Helena.Test.Back.API/Program.cs
@@ -8,9 +8,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(g => g.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml")));
 builder.Services.AddServicesAndRepositories();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors();
 
 if (app.Environment.IsDevelopment())
 {
